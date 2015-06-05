c749d4f794b81a2ed138cfcd15d88943035eb9fa
Stephen DiIorio <stephendiiorio@Stephens-MacBook-Pro-2.local>
2015-06-05 01:47:48 -0400

    added some documentation

--- cacheSweeper.py
+++ cacheSweeper.py
@@ -1,3 +1,9 @@
+# Authors: Stephen DiIorio and Andrew Ivarson
+# Date: June 2015
+#
+# This is our final project for CSc 483 - Advanced Hardware, where we
+# try to implement a fun game.
+
 print "Enter memory address"
 addr = int(input())
 print addr
